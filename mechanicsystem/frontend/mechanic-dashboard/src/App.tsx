0a939673e20e61ba3d27d6328ff12f6e1aa4ec18
Gabriel Gallo <155118400+GabrielGallo1@users.noreply.github.com>
2025-12-29 15:13:24 -0300

    feat: Frontend iniciated, componets starting

--- mechanicsystem/frontend/mechanic-dashboard/src/App.tsx
+++ mechanicsystem/frontend/mechanic-dashboard/src/App.tsx
@@ -1,6 +1,26 @@
 
-function App() {
-  return <h1> Mechanic Dash</h1>
+function App() { 
+  return (
+      <main>
+        <div className="container">
+          <div className="box">
+              <div className="boxImage1"></div>
+              <a className="fontBackground">Client</a>
+          </div>
+
+          <div className="box">
+              <div className="boxImage1"></div>
+              <a className="fontBackground">Workorders</a>
+          </div>
+
+            <div className="box">
+              <div className="boxImage1"></div>
+              <a className="fontBackground">Mechanic System</a>
+          </div>
+
+        </div>
+      </main>
+  )
 }
 
-export default App
+export default App;
