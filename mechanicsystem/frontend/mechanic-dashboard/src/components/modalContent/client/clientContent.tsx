342366f4ac7e74e2b4a3c091e44ca79c7ddff01e
Gabriel Gallo <155118400+GabrielGallo1@users.noreply.github.com>
2026-01-09 15:17:31 -0300

    Hell Hein

--- mechanicsystem/frontend/mechanic-dashboard/src/components/modalContent/client/clientContent.tsx
+++ mechanicsystem/frontend/mechanic-dashboard/src/components/modalContent/client/clientContent.tsx
@@ -4,9 +4,38 @@ import styles from './clientContent.module.css';
 function ClientContent() {
   return (
     <div className={styles.container}>
-      <h2 className={styles.h2}>Client System Modal Content</h2>
       <div className={styles.boxContent}>
-        <p>Client registration form will go here.</p>
+        <div className={styles.boxTitle}>
+          <h2 className={styles.h2}>Client System Modal Content</h2>
+        </div>
+        <div className={styles.boxForms}>
+          <div className={styles.boxLabel}>
+            <label className={styles.label}> 
+              <p>dale</p>
+            </label>
+            <label className={styles.label}> 
+              <p>dale</p>
+            </label>
+          </div>
+          <div className={styles.boxInput}>
+            <input className={styles.inputLarge1} type="text" placeholder="Name" />
+            <input className={styles.inputSmall1} type="text" placeholder="Adress" />
+          </div>
+        </div>
+        <div className={styles.boxForms}>
+          <div className={styles.boxLabel}>
+            <label className={styles.label}> 
+              <p>dale</p>
+            </label>
+            <label className={styles.label}> 
+              <p>dale</p>
+            </label>
+          </div>
+          <div className={styles.boxInput}>
+            <input className={styles.inputLarge2} type="text" placeholder="Phone" />
+            <input className={styles.inputSmall2} type="text" placeholder="Document" />
+          </div>
+        </div>
       </div>
       <div className={styles.boxContent}>
         <p>Client registration form will go here.</p>
